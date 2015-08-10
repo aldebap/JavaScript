e119c79a7d799aa91881cbd43a9008fb1e83a52f
aldebap <aldeba_bla@yahoo.com>
2015-08-10 18:25:44 -0300

    Create chapter3.js

---
+++ chapter3.js
@@ -0,0 +1,10 @@
+/* *
+	 * Funcao de Teste numero 1
+	 */
+
+function funcaoUm() {
+	var console = document.getElementById( 'console' );
+	var inteiro_positivo = 125;
+
+	console.innerHTML = "inteiro (positivo): " + inteiro_positivo + "<br />";
+}
