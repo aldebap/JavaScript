32d77da51d02a57f5a78235f5781e40d47c168c8
aldebap <aldeba_bla@yahoo.com>
2017-08-24 10:53:34 -0300

    Update calculatePi.js
    Use of Math library to make code simpler

--- calculatePi.js
+++ calculatePi.js
@@ -1,23 +1,9 @@
 "use strict";
 
-//	exponentiation function (the operator ** is giving me syntax error)
-function power( _base, _exponent ) {
-
-	let	result = 1;
-
-	for( let i = 1; i <= _exponent; i++ ) {
-		result *= _base;
-	}
-
-	return result;
-}
-
 //	calculatePi.js - an app to calculate the Pi value
 let		pi = 0;
 const	precision = 8;
-//	TODO: fix it to use the exponentiation ** operator
-//const	difference = 1 / ( 10 ** ( precision + 1 ) );
-const	difference = 1 / power( 10, precision + 1 );
+const	difference = 1 / Math.pow( 10, precision + 1 );
 let		i;
 
 console.info( `difference = ${difference}` );
@@ -32,18 +18,13 @@ for( i = 1;; i++ ) {
 		pi -= 1 / ( 2 * i - 1 );
 	}
 
-	//	TODO: this test should be easier if I knew a function to get the Abs() of a number
-	if( pi > previousPi ) {
-		if( 4 * ( pi - previousPi ) < difference ) {
-			break;
-		}
-	} else {
-		if( 4 * ( previousPi - pi ) < difference ) {
-			break;
-		}
+	if( 4 * Math.abs( pi - previousPi ) < difference ) {
+		break;
 	}
 }
 
+pi = ( 4 * pi ).toFixed( precision );
+
 console.info( `i = ${i}` );
 
-console.info( `Pi = ${ 4 * pi}` );
+console.info( `Pi = ${pi}` );
